7eb717a14fb51ecca252f536776b1e66d13ce9f0
Aneeb Baig <aneebbaig123@gmail.com>
2023-08-29 15:57:35 +0500

    implemented all the views for main screen

--- lib/responsiveness/responsive_widget.dart
+++ lib/responsiveness/responsive_widget.dart
@@ -4,21 +4,28 @@ import 'responsive_sizes.dart';
 
 class ResponsiveWidget extends StatelessWidget {
   final Map<ResponsiveSize, Widget> responsiveMap;
+  final String? text;
 
   const ResponsiveWidget({
     Key? key,
     required this.responsiveMap,
+    this.text,
   }) : super(key: key);
 
   @override
    Widget build(BuildContext context) {
     return LayoutBuilder(
+      
       builder: (context, constraints) {
         final screenWidth = constraints.maxWidth;
         final currentSize = determineResponsiveSize(screenWidth);
+        if(text!=null){
+          print(constraints.maxWidth);
+        }
 
         return responsiveMap[currentSize]!;
       },
+      
     );
   }
 }
